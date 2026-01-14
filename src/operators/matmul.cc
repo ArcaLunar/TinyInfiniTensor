8521621d0a4bf294e9e643ea9ac3a24cbeb270df
ArcaLunar <arca@connect.hku.hk>
2026-01-14 23:49:48 +0800

    hw7: matmul shape inference

--- src/operators/matmul.cc
+++ src/operators/matmul.cc
@@ -1,21 +1,48 @@
 #include "operators/matmul.h"
+#include "utils/operator_utils.h"
+#include <algorithm>
+#include <cassert>
 
 namespace infini {
 
-  MatmulObj::MatmulObj(GraphObj *graph, Tensor A, Tensor B, Tensor C, bool transA, bool transB) : OperatorObj(OpType::MatMul, TensorVec{A, B}, {C}), transA(transA), transB(transB) { IT_ASSERT(checkValid(graph)); }
+MatmulObj::MatmulObj(GraphObj *graph, Tensor A, Tensor B, Tensor C, bool transA,
+                     bool transB)
+    : OperatorObj(OpType::MatMul, TensorVec{A, B}, {C}), transA(transA),
+      transB(transB) {
+  IT_ASSERT(checkValid(graph));
+}
 
-  string MatmulObj::toString() const {
-    std::ostringstream os;
-    os << "Matmul([" << (transA ? "A^T" : "A") << "," << (transB ? "B^T" : "B]") << ",A=" << inputs[0]->getGuid() << ",B=" << inputs[1]->getGuid() << ",C=" << outputs[0]->getGuid() << ",mnk=[" << m << "," << n << "," << k << "])";
-    return os.str();
-  }
+string MatmulObj::toString() const {
+  std::ostringstream os;
+  os << "Matmul([" << (transA ? "A^T" : "A") << "," << (transB ? "B^T" : "B]")
+     << ",A=" << inputs[0]->getGuid() << ",B=" << inputs[1]->getGuid()
+     << ",C=" << outputs[0]->getGuid() << ",mnk=[" << m << "," << n << "," << k
+     << "])";
+  return os.str();
+}
 
-  optional<vector<Shape>> MatmulObj::inferShape(const TensorVec &inputs) {
-    // =================================== 作业 ===================================
-    // TODO：返回经过 matmul 操作后的 shape
-    // REF: https://github.com/onnx/onnx/blob/main/docs/Operators.md#gemm
-    // =================================== 作业 ===================================
-    return std::nullopt;
-  }
+optional<vector<Shape>> MatmulObj::inferShape(const TensorVec &inputs) {
+  // TODO：返回经过 matmul 操作后的 shape
+  // REF: https://github.com/onnx/onnx/blob/main/docs/Operators.md#gemm
+  auto A = inputs[0]->getDims();
+  auto B = inputs[1]->getDims();
+  std::reverse(A.begin(), A.end()), std::reverse(B.begin(), B.end());
+
+  if (transA)
+    std::swap(A[0], A[1]);
+  if (transB)
+    std::swap(B[0], B[1]);
+
+  while (A.size() < B.size())
+    A.push_back(1);
+  while (B.size() < A.size())
+    B.push_back(1);
+
+  for (int i = 2, rA = A.size(); i < rA; i++)
+    A[i] = std::max(A[i], B[i]); // broadcast
+  A[0] = B[0];
+  std::reverse(A.begin(), A.end());
+  return {{A}};
+}
 
 } // namespace infini
